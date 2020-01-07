5452be4e8b08ee7b1e5ca21b1e8600b9f2749097
Hamza Zakaria <hamza.zakaria9@gmail.com>
2020-01-07 11:22:02 +0100

    refactoring function computeText

--- src/TemplateManager.php
+++ src/TemplateManager.php
@@ -29,54 +29,51 @@ class TemplateManager
     {
         $APPLICATION_CONTEXT = ApplicationContext::getInstance();
 
-        $quote = (isset($data['quote']) and $data['quote'] instanceof Quote) ? $data['quote'] : null;
+        $quote = (isset($data['quote']) && $data['quote'] instanceof Quote) ? $data['quote'] : null;
 
         if ($quote)
         {
-            $_quoteFromRepository = QuoteRepository::getInstance()->getById($quote->id);
-            $usefulObject = SiteRepository::getInstance()->getById($quote->siteId);
-            $destinationOfQuote = DestinationRepository::getInstance()->getById($quote->destinationId);
+            $site = SiteRepository::getInstance()->getById($quote->siteId);
+            $destination = DestinationRepository::getInstance()->getById($quote->destinationId);
+            $quote = QuoteRepository::getInstance()->getById($quote->id);
 
-            if(strpos($text, '[quote:destination_link]') !== false){
-                $destination = DestinationRepository::getInstance()->getById($quote->destinationId);
+
+            if (strpos($text, '[quote:destination_link]') !== false) {
+                $url = '';
+                if ($destination) {
+                    $url = $site->url . '/' . $destination->countryName . '/quote/' . $quote->id;
+                }
+                $text = str_replace('[quote:destination_link]', $url, $text);
             }
 
-            $containsSummaryHtml = strpos($text, '[quote:summary_html]');
-            $containsSummary     = strpos($text, '[quote:summary]');
-
-            if ($containsSummaryHtml !== false || $containsSummary !== false) {
-                if ($containsSummaryHtml !== false) {
-                    $text = str_replace(
-                        '[quote:summary_html]',
-                        Quote::renderHtml($_quoteFromRepository),
-                        $text
-                    );
-                }
-                if ($containsSummary !== false) {
-                    $text = str_replace(
-                        '[quote:summary]',
-                        Quote::renderText($_quoteFromRepository),
-                        $text
-                    );
-                }
+            if (strpos($text, '[quote:summary_html]') !== false) {
+                $text = str_replace(
+                    '[quote:summary_html]',
+                    Quote::renderHtml($quote),
+                    $text
+                );
             }
 
-            (strpos($text, '[quote:destination_name]') !== false) and $text = str_replace('[quote:destination_name]',$destinationOfQuote->countryName,$text);
+            if (strpos($text, '[quote:summary]') !== false) {
+                $text = str_replace(
+                    '[quote:summary]',
+                    Quote::renderText($quote),
+                    $text
+                );
+            }
+
+            if (strpos($text, '[quote:destination_name]') !== false) {
+                $text = str_replace('[quote:destination_name]', $destination->countryName, $text);
+            }
         }
 
-        if (isset($destination))
-            $text = str_replace('[quote:destination_link]', $usefulObject->url . '/' . $destination->countryName . '/quote/' . $_quoteFromRepository->id, $text);
-        else
-            $text = str_replace('[quote:destination_link]', '', $text);
+        $_user  = (isset($data['user'])  && ($data['user']  instanceof User))  ? $data['user'] : $APPLICATION_CONTEXT->getCurrentUser();
 
-        /*
-         * USER
-         * [user:*]
-         */
-        $_user  = (isset($data['user'])  and ($data['user']  instanceof User))  ? $data['user']  :
-            $APPLICATION_CONTEXT->getCurrentUser();
         if($_user) {
-            (strpos($text, '[user:first_name]') !== false) and $text = str_replace('[user:first_name]'       , ucfirst(mb_strtolower($_user->firstname)), $text);
+            if(strpos($text, '[user:first_name]') !== false)
+            {
+                $text = str_replace('[user:first_name]', ucfirst(mb_strtolower($_user->firstname)), $text);
+            }
         }
 
         return $text;
